9420cf741196b220e8a5b28a412095bb9ed28a9a
Wade Kallhoff <shagler@gmail.com>
2019-04-08 14:24:13 -0500

    Updating to latest

--- src/config.default.ts
+++ src/config.default.ts
@@ -4,10 +4,10 @@ import { ScreenshotConfig } from "./utilities/screenshot/screenshot-utility";
 
 export class Config {
     public historyPersistenceStorageFile: string = path.join(os.homedir(), ".am/activity.db");
-    public timeBetweenWindowLoggingInSeconds: number = 5;
-    public idleThresholdInSeconds: number = 30;
+    public timeBetweenWindowLoggingInSeconds: number = 15;
+    public idleThresholdInSeconds: number = 120;
     public idlePollIntervalInSeconds: number = 5;
-    public timeBetweenScreenshotsInSeconds: number = 30;
+    public timeBetweenScreenshotsInSeconds: number = 60;
     public screenshotStorageDirectory: string = path.join(os.homedir(), ".am/Screenshots");
     public screenshotImageSettings: ScreenshotConfig = {
         width: 900,
